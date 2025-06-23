3e8ead73720b95ad6a72bd33266d89f0e90cbf16
Yevgen Shymka <schimka3@gmail.com>
2025-06-23 17:26:42 +0200

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import Button from "./Button";
+
+export default function App() {
+  return (
+    <>
+      <Button variant="primary" text="Login" />
+      <Button variant="secondary" text="Follow" />
+    </>
+  );
+}

cec603fbe91d7d32f776a21fad673b58c674d15b
Yevhen Shymka <schimka3@gmail.com>
2025-06-23 22:56:17 +0200

    caffe

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import "modern-normalize/modern-normalize.css";
 
-const techName = "React";
-const imgUrl =
-  "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?w=640";
-
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <>
-    <h1>Welcome to {techName}</h1>
-    <p>
-      This is JSX — it looks like HTML, but it's not quite the same. It has its
-      own rules!
-    </p>
-    <img src={imgUrl} alt="Man, field and a mountain" width="640" />
-  </>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
